f6c075ee916c323c698addce98f8cd5656625365
Aaron Hedges <aaron@dashron.com>
2020-08-07 20:01:58 -0400

    fix up the token generation, and move off of axios

--- src/tests/__tests__/test.ts
+++ src/tests/__tests__/test.ts
@@ -1,15 +1,30 @@
 "use strict";
 
-import Request from '../../index';
+import Client from '../../index';
+let config = require('../config.json');
 
 describe('request tests', () => {
 	/**
 	 *
 	 */
-	test.todo('TODO', () => {
-		// expect.assertions(1);
-		// let foo = Request();
+	test('requestToken', () => {
+		expect.assertions(2);
+		let foo = new Client(config.apiKey, config.apiSecret);
 
-		// expect(foo).toEqual(undefined);
+		return foo.requestToken(config.username, config.password)
+		.then((res) => {
+			expect(res.res.status).toEqual(200);
+			expect(res.json).toHaveProperty('access_token');
+		});
+	});
+
+	test('get', () => {
+		expect.assertions(2);
+
+		const client = new Client(config.apiKey, config.apiSecret, config.accessToken);
+		return client.get('/film/lnXw/statistics').then(res => {
+			expect(res.res.status).toEqual(200);
+			expect(res.json.film.id).toEqual('lnXw');
+		});
 	});
 });
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,16 +1,15 @@
 export default class Client {
     private accessToken;
-    private req;
     private apiKey;
     private apiSecret;
     constructor(apiKey: string, apiSecret: string, accessToken?: string);
-    private requireAccessToken;
+    get(path: string, params?: object): any;
+    post(path: string, body: object, headers?: object): any;
+    patch(path: string, body: object): any;
+    delete(path: string): any;
+    requestToken(username: string, password: string): any;
     private request;
-    get(path: string, params?: object): Promise<import("axios").AxiosResponse<any>>;
-    post(path: string, body: object, headers?: object): Promise<import("axios").AxiosResponse<any>>;
-    patch(path: string, body: object): Promise<import("axios").AxiosResponse<any>>;
-    delete(path: string): Promise<import("axios").AxiosResponse<any>>;
+    private applyAuth;
     private params;
     private buildUrl;
-    requestToken(username: string, password: string): Promise<import("axios").AxiosResponse<any>>;
 }
